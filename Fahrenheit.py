c053c58e79fbdf5741e49d569032e23506091c86
Shourya-rath <166209675+Shourya-rath@users.noreply.github.com>
2024-09-11 12:50:10 +0530

    Update Fahrenheit.py

--- Fahrenheit.py
+++ Fahrenheit.py
@@ -1,30 +1,43 @@
-import tkinter as tk from tkinter import messagebox
+#include <stdio.h>
+int arr[10][10][10][10];
+int dim[4];
 
-def fahrenheit_to_celsius():
+int main() {
+  int x,y,z,w,value ;
+  char choice;
+  printf("Enter the dimensions of the 4D array:\n");
+  printf("Dimension 1:");
+  scanf("%d",&dim[0]);
+  printf("Dimension 2:");
+  scanf("%d",&dim[1]);
+  printf("Dimension 3:");
+  scanf("%d",&dim[2]);
+  printf("Dimension 4:");
+  scanf("%d",&dim[3]);
+  for(int i = 0 ; i< dim[0] ;i++){
+      for(int j = 0 ; j< dim[1] ;j++)
+        for(int k = 0 ; k< dim[2] ;k++)
+        for(int i = 0 ; i< dim[0] ;i++)
+        arr[i][j][k][l] = 0 ;
+        }
+do{
+printf("Enter position(x,y,z,w)and value to insert(format: x,y,z,w value)");
+scanf("%d %d %d %d %d ",&x,&y,&z,&w,&value) ;
+if(x >= dim[0]||y >= dim[1]||z >= dim[2]||w >= dim[3]||x<0||y<0||   z<0||w<0){
+    printf("Error : position out of bounds.\n");
+}
 
-{x}
+else{
+  arr[x][y][z][w] = value ;
+  printf("Value %d inserted at position [%d][%d][%d][%d]\n",value,x,y,z,w);
+}
+printf("Do you want to insert another element?(y/n):");
+scanf("%c",&choice);
+}while(choice == 'y'|| choice == 'Y'){
+    printf("Arraycontents: \n");
+    
+}
 
-fahrenheit entry.get()
-
-if fahrenheit.replace('.,", 1).isdigit() or (fahrenheit.s fahrenheit = float(fahrenheit) celsius (fahrenheit 32) 5/9
-
-messagebox.showinfo("Result", f"{fahrenheit) Fahrenheit
-
-else:
-
-messagebox.showerror("Invalid input", "Please enter a val root = tk. Tk()
-
-root.title("Fahrenheit to Celsius Converter")
-
-label tk.Label(root, text="Enter temperature in Fahrenheit:")
-
-label.pack(pady=10)
-
-entry tk. Entry (root)
-
-entry.pack(pady=5)
-button = tk.Button(root, text="Convent", command=fahrenheit_to_celsi
-
-button.pack(pady-10)
-
-root.mainloop()
+  
+    return 0;
+}
